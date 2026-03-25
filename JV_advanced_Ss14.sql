98c2ae571378377b8de2d65dc2ee8a327086def6
Dung246 <dungoo2605@gmail.com>
2026-03-25 14:05:19 +0700

    Update JV_advanced_Ss14.sql

--- JV_advanced_Ss14.sql
+++ JV_advanced_Ss14.sql
@@ -1,18 +1,26 @@
 CREATE DATABASE tranfer_db;
-use tranfer_db;
+USE tranfer_db;
+
 CREATE TABLE Accounts(
-AccountId VARCHAR(10) PRIMARY KEY,
-FullName VARCHAR(50),
-Balance DECIMAL(18, 2)
+    AccountId VARCHAR(10) PRIMARY KEY,
+    FullName VARCHAR(50),
+    Balance DECIMAL(18, 2)
 );
 
-INSERT INTO Accounts VALUES('ACC01', 'NguyenVanA', 5000), ('ACC02', 'Tran Thi B',2000)
+INSERT INTO Accounts VALUES
+('ACC01', 'Đỗ Thị Thuỳ Trang', 5000),
+('ACC02', 'Trần Mạnh Hồng', 2000);
+
 DELIMITER //
+
 CREATE PROCEDURE sp_UpdateBalance(
-@Id VARCHAR(10),
-@Amount DECIMAL(18,2)
+    IN Id VARCHAR(10),
+    IN Amount DECIMAL(18,2)
 )
-AS
 BEGIN
-UPDATE Accounts SET Balance=Balance+@Amount WHERE A	ccountId=@Id;
-END//
+    UPDATE Accounts
+    SET Balance = Balance + Amount
+    WHERE AccountId = Id;
+END //
+
+DELIMITER ;
